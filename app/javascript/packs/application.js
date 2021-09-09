dc42d9766e4926a7420f6fced3a31e538043e4b9
azmamza <87520471+azmamza@users.noreply.github.com>
2021-09-09 14:22:37 +0900

    jQuery Ratyの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
-
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,4 +15,3 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery.raty.js
