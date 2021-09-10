ec7c43680eb69d829aec13638a44f889b889ad9b
azmamza <87520471+azmamza@users.noreply.github.com>
2021-09-10 12:55:49 +0900

    postsコントローラーにアクションを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,33 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+# def edit
+#   @post = Post.find(params[:id])
+#   redirect_to action: :index unless @post.user.id == current_user.id
+# end
+
+# def search
+#   @posts = Post.search(params[:keyword])
+# end
+
+private
+
+def post_params
+  params.require(:post).permit(:name, :genre_id, :rating, :title, :review).merge(user_id: current_user.id)
 end
