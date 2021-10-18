b602cab42b95fdd8ade5d159d422216bd2ecfb9b
azmamza <87520471+azmamza@users.noreply.github.com>
2021-10-18 17:28:16 +0900

    userモデルのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   has_many :posts, dependent: :destroy
+
+  validates :nickname, presence: true
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
+    validates :password
+    validates :password_confirmation
+  end
+
 end
